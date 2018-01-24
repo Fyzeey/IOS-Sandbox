e2ebe5ce7aa98e61d06e577fd7bb8d5213d9d187
Yinze Fan <fanyinze@gmail.com>
2018-01-24 15:33:26 -0500

    Update Perform_Segueway.swift

--- Recipes/Perform_Segueway.swift
+++ Recipes/Perform_Segueway.swift
@@ -1,4 +1,6 @@
-//==============Segueway Setup===============
+///////////////////////Segueway Setup\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+
+//////////////////////From First VC to Second VC\\\\\\\\\\\\\\\\\\\\\\
 
 // Create a second view controller
 
@@ -33,5 +35,14 @@ override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
 var textPassedOver : String?
 
 
+//////////////////////////From Second back to First\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+// 1 create protol in second VC
+// 2 conform protol in first VC
+// 3 write required delegate method in first VC, do something with received data inside delegate method.
+// 4 create new property inside second VC called delegate of type protocol with "?"
+// 5 in @IBAction of button(second VC) which you want send databack. Put following code
+   delegate?.dataReceived(data: textField.text!) //dataReceived is delegate method
+   dismiss(animated: true, completion: nil)
+// 6 inside first VC, in the prepare function, write secondVC.delegate = self, where let secondVC = segue.destination as! SecondViewController is already defined.
 
 
